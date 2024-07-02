369368f2fbbbcd8d25e75c799988810a42817736
Siddico <www.mohammedahmedmohammedsiddiq@gmail.com>
2024-07-02 12:42:55 +0300

    add custom horizontal list view item to home view

--- bookly/lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ bookly/lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/Core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_appbar.dart';
+import 'custom_horizontal_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -8,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [const CustomAppBar()],
+      children: const [const CustomAppBar(), CustomHorizontalListViewItem()],
     );
   }
 }

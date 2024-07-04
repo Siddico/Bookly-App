889017b6cc2c814401b6daf263d2fdb50f6898f9
Siddico <www.mohammedahmedmohammedsiddiq@gmail.com>
2024-07-04 18:29:49 +0300

    create similar books list view

--- bookly/lib/Features/home/presentation/views/widgets/book_datails_view_body.dart
+++ bookly/lib/Features/home/presentation/views/widgets/book_datails_view_body.dart
@@ -1,11 +1,11 @@
 import 'package:bookly/Core/utils/styles.dart';
-import 'package:bookly/Core/widgets/custom_action_button.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/rating_and_its_number.dart';
 import 'package:flutter/material.dart';
 
 import 'books_action.dart';
 import 'custom_book_details_appbar.dart';
 import 'custom_horizontal_list_view_item.dart';
+import 'similar_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -15,7 +15,7 @@ class BookDetailsViewBody extends StatelessWidget {
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
+      child: ListView(
         children: [
           const SizedBox(
             height: 30,
@@ -31,19 +31,23 @@ class BookDetailsViewBody extends StatelessWidget {
           const SizedBox(
             height: 43,
           ),
-          Text(
-            'The Junngle Book',
-            style: styles.textStyle30,
+          Center(
+            child: Text(
+              'The Junngle Book',
+              style: styles.textStyle30,
+            ),
           ),
           const SizedBox(
             height: 6,
           ),
           Opacity(
             opacity: 0.7,
-            child: Text(
-              'Rudyard kipling',
-              style: styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            child: Center(
+              child: Text(
+                'Rudyard kipling',
+                style: styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+              ),
             ),
           ),
           const SizedBox(
@@ -55,7 +59,24 @@ class BookDetailsViewBody extends StatelessWidget {
           const SizedBox(
             height: 37,
           ),
-          const BooksAction()
+          const BooksAction(),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You Can Also Like',
+              style: styles.textStyle14.copyWith(fontWeight: FontWeight.w700),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksListView(),
+          const SizedBox(
+            height: 40,
+          )
         ],
       ),
     );
---
+++ bookly/lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'custom_horizontal_list_view_item.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.135,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.0),
+            child: CustomHorizontalListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}

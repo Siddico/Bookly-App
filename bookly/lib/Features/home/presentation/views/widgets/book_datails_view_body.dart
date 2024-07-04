fde4260a5afe963698ee1519428fc3e01b16d84d
Siddico <www.mohammedahmedmohammedsiddiq@gmail.com>
2024-07-04 16:58:54 +0300

    add custom horizontal list view item to book details view

--- bookly/lib/Features/home/presentation/views/widgets/book_datails_view_body.dart
+++ bookly/lib/Features/home/presentation/views/widgets/book_datails_view_body.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_appbar.dart';
+import 'custom_horizontal_list_view_item.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           const SizedBox(
             height: 30,
           ),
-          const CustomBookDetailsAppBar()
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .18),
+            child: const CustomHorizontalListViewItem(),
+          ),
         ],
       ),
     );

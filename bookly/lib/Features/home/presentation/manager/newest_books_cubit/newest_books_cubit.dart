17bfec9ec747dc1d02ba3eae91b9ae0e8518646c
Siddico <www.mohammedahmedmohammedsiddiq@gmail.com>
2024-07-10 12:51:45 +0300

    newest books cubit done

---
+++ bookly/lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
+import 'package:bookly/Features/home/presentation/manager/newest_books_cubit/newest_books_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksStates> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
